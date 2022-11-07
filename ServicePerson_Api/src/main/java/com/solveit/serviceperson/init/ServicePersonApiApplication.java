83216979c0b5f3b1a330705f942da05edf577209
juanma1231 <juan.zuluaga4461@uco.net.co>
2022-11-07 14:22:08 -0500

    first commit

---
+++ ServicePerson_Api/src/main/java/com/solveit/serviceperson/init/ServicePersonApiApplication.java
@@ -0,0 +1,13 @@
+package com.solveit.serviceperson.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePersonApiApplication.class, args);
+	}
+
+}
---
+++ ServicePerson_Api/src/test/java/com/solveit/serviceperson/init/ServicePersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solveit.serviceperson.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
