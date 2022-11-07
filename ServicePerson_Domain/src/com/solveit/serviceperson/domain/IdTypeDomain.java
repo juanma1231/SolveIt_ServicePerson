97abfe166b81cc9f161e06b3c5ab3e718905c9f0
juanma1231 <juan.zuluaga4461@uco.net.co>
2022-11-07 15:50:35 -0500

    second

---
+++ ServicePerson_Domain/src/com/solveit/serviceperson/domain/IdTypeDomain.java
@@ -0,0 +1,35 @@
+package com.solveit.serviceperson.domain;
+
+import java.util.UUID;
+
+public class IdTypeDomain {
+	private UUID id;
+	private String description;
+	
+	
+	
+	public IdTypeDomain(UUID id, String description) {
+		super();
+		this.id = id;
+		this.description = description;
+	}
+	
+	public static final IdTypeDomain create(final UUID id, final String description) {
+		return new IdTypeDomain(id,  description);
+	}
+	public final UUID getId() {
+		return id;
+	}
+	public final void setId(UUID id) {
+		this.id = id;
+	}
+	public final String getDescription() {
+		return description;
+	}
+	public final void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
